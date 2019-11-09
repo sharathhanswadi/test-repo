cfb842a7a290ca46bf77f8ea0966b54daf836680
Your Name <shibir@triconinfotech.com>
2019-11-09 15:23:39 +0530

    local

--- porch/src/index.js
+++ porch/src/index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/locations', (req, res) => {
   res.status(200).json([
     {
-      street1: 'sharath done  ',
+      street1: 'final istio done  ',
       street2: 'Hosur road',
       city: 'Bangalore',
       pincode: '560095'
